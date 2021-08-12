ad73980aa8a7a814e51a396b27c848dee63ff0e4
TarunJuluru-nc14977 <tarunjuluru@ninjacart.com>
2021-08-12 22:29:34 +0530

    basic structure ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Editor from "./components/Editor";
 
 function App() {
+  let options = {
+    lineNumbers: true,
+    theme: 'material',
+  }
+  let javascript = { ...options, mode: 'javascript' }
+  let html = { ...options, mode: 'xml' };
+  let css = { ...options, mode: "text/css" };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-pane border">
+        <Editor options= {html}/*HTML code editor*/ />
+        <Editor options = {css}/*CSS code editor*/ />
+        <Editor options = {javascript}/*JS code editor*/ />
+      </div>
+      <div className="bottom-pane border">
+        <iframe
+          title="myCode"
+          className="iframe"
+          /*Title option*/
+          /*sandbox option*/
+          /*frameBorder option*/
+          /*width-height option*/
+          /*Title option*/
+        ></iframe>
+      </div>
     </div>
   );
 }
---
+++ src/components/Editor.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Controlled as CodeMirror } from "react-codemirror2";
+require("codemirror/lib/codemirror.css");
+require("codemirror/theme/material.css");
+require("codemirror/mode/xml/xml.js");
+require("codemirror/mode/css/css.js");
+require("codemirror/mode/javascript/javascript.js");
+
+
+
+class Editor extends React.Component{
+    constructor() {
+        super();
+        console.log("setting state")
+        this.state = { value: 'hello' };
+        
+    }
+    render() {
+        console.log(this.state)
+        return (
+          <div className="border editor">
+            <div>
+              {this.props.options.mode}
+              <span className="minimise-button">X</span>
+            </div>
+            <CodeMirror
+              value={this.state.value}
+              options={this.props.options}
+              onBeforeChange={(editor, data, value) => {
+                this.setState({ value });
+              }}
+              onChange={(editor, data, value) => {
+              }}
+            />
+          </div>
+        );
+}
+}
+export default Editor;
+
+
+
